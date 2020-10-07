08bf056b641799a31e17420a5a5c519c388f44ed
tawaji <tawaji@bemap.co.jp>
2020-10-07 18:28:42 +0900

    不要要素の削除

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,11 +1,8 @@
-import React from "react"
-import {Link} from "gatsby"
-import ThemeChanger from "../components/themeChanger"
+import React from "react";
+import ThemeChanger from "../components/themeChanger";
 
 export default (props) => (
-  <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
-    <ThemeChanger/>
+  <nav className="navigation">
+    <ThemeChanger />
   </nav>
-  
-)
+);
